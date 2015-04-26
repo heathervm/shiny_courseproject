022bac29d858cbe8ba1fb9f03e5b9fe7ee8067fb
heathervm <heathervmurray@gmail.com>
2015-04-25 21:57:39 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,13 +1,21 @@
 library(shiny)
 shinyUI(fluidPage(
-  titlePanel("Courseproject"),
+  titlePanel("Course Project"),
   sidebarLayout(
-    sidebarPanel("sidebar",
+    sidebarPanel("Slide these two sliders to pick numbers to add together",
+    
+    
     sliderInput("n_1", label = "First Number", min = 0, max = 100, value = 5, step = 1),
-    sliderInput("n_2", label = "Number2", min = 0, max = 100, value = 5, step = 1)
+    sliderInput("n_2", label = "Second Number", min = 0, max = 100, value = 5, step = 1)
   ),
-        mainPanel("Your Sum Is",
+        mainPanel(
+        h1("Heather Murray's Course Project"),  
+        h3("Welcome to my course project. I made an app to add numbers, because math is hard"),
+        h4("below you'll find the sum of the two sliders you adjust on the sidebar"),
+        p("because let's face it, you don't have that many fingers, and toes are often covered with shoes"),
+        
         textOutput("text1")
+        
         )
         )
 )
